1ec8a98ada103a3287c2f2442668eb28799dd910
odsproject095 <odsproject095@gmail.com>
2018-06-06 17:46:57 +0700

    second commit

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 
+import { Container } from 'semantic-ui-react';
+import NavBar from '../../features/nav/NavBar/NavBar';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+
 class App extends Component {
   render() {
-    return <h1>Welcome to Re-vents</h1>;
+    return (
+      <div>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </div>
+    );
   }
 }
 
